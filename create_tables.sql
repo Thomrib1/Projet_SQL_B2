419a0d5ed0cadc4db64ef4ee8847ea368405dbfe
Thomas <thomas.ribeiro@ynov.com>
2025-12-10 11:41:42 +0100

    add table clients

--- create_tables.sql
+++ create_tables.sql
@@ -9,3 +9,13 @@ CREATE TABLE Vehicule (
     etat VARCHAR(30),
     dernier_controle DATE
 );
+
+CREATE TABLE CLients (
+    id_client
+    Nom VARCHAR(50) NOT NULL,
+    Prenom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telephone VARCHAR(20) NOT NULL,
+    date_inscription DATE,
+    validite_permis DATE
+)
