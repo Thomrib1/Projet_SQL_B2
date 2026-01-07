9edea20902a0c3a6f2a4705ed6f64039b28a7817
Thomrib1 <thomas.ribeiro@ynov.com>
2026-01-07 21:08:19 +0100

    Delete SQL/INSERT directory

--- SQL/INSERT/insert-bornes_recharge.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO BORNES_DE_RECHARGE (puissance_kw, statut_borne, type_connecteur, id_station) VALUES
-(50, 'Disponible', 'Type 2', 1),
-(150, 'Disponible', 'CCS Combo', 1),
-(22, 'En maintenance', 'Type 2', 2),
-(50, 'Disponible', 'Type 2', 3),
-(100, 'Hors service', 'CCS Combo', 5);
