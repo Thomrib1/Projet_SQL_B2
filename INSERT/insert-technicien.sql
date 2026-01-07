db0b59fdcb0de5b36dc057912a99f463cdb7b7d5
Thomrib1 <thomas.ribeiro@ynov.com>
2026-01-07 18:41:09 +0100

    Delete INSERT directory

--- INSERT/insert-technicien.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TECHNICIENS (Nom, Prenom, email, telephone, date_embauche, statut) VALUES
-('Durand', 'Paul', 'p.durand@garage.com', '0701020304', '2022-06-01', 'Actif'),
-('Leroy', 'Alice', 'a.leroy@garage.com', '0711223344', '2023-01-15', 'Actif'),
-('Moreau', 'Kevin', 'k.moreau@garage.com', '0722334455', '2021-09-10', 'Indisponible'),
-('Simon', 'Claire', 'c.simon@garage.com', '0733445566', '2023-05-20', 'Actif'),
-('Giraud', 'Alain', 'a.giraud@garage.com', '0744556677', '2020-11-30', 'Actif');
--- INSERT/insert-transaction.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO TRANSACTIONS (montant_transaction, moyen_paiement, statut_transaction, id_paiement) VALUES
-(45.00, 'Carte Bancaire', 'Succès', 1),
-(110.00, 'PayPal', 'Succès', 2),
-(55.00, 'Carte Bancaire', 'En attente', 3),
-(90.00, 'Carte Bancaire', 'Succès', 4),
-(40.00, 'Apple Pay', 'Succès', 5);
