115e9873cd7881f25c87cb14c50be30a3d0fbd87
nakchaya <akchayan.nihalananth@ynov.com>
2026-01-07 21:37:27 +0100

    ajout de requetes en 1 seul fichier avec explications

--- query.sql
+++ query.sql
@@ -26,6 +26,7 @@ FROM STATIONS s
 LEFT JOIN VEHICULES v ON s.lieu = v.localisation    -- jointure entre stations et véhicules selon lieu/localisation
 GROUP BY s.id_station, s.Nom, s.capacite_max_vehicules, s.lieu;     -- regroupé par id_station, nom, capacité max, lieu
 
+
 -- 3. Moyenne de prix et de durée par marque de véhicule
 SELECT 
     v.marque, 
@@ -38,27 +39,50 @@ LEFT JOIN PAIEMENTS p ON l.id_location = p.id_location                       -- 
 GROUP BY v.marque, v.modele                                     -- regroupé par marque et modèle    
 ORDER BY cout_moyen DESC;                                   
 
+
 -- 4. Nbr de réservations par client
 SELECT clients.Nom, clients.Prenom, COUNT(id_reservation) AS reservation_par_clients    -- sélectionne nom, prénom et compte nbr de réservations par client
 FROM reservations
 JOIN clients ON clients.id_client = reservations.id_client  -- jointure entre clients et réservations selon id_client
 GROUP BY clients.id_client, clients.Nom, clients.Prenom;    -- regroupé par id_client, nom et prénom
 
+
 -- 5. Interventions des techniciens et véhicules concernés
 SELECT t.nom, t.prenom, t.statut, COUNT(m.id_maintenance) AS nbr_interventions, STRING_AGG(CAST(m.id_vehicule AS TEXT), ', ') AS vehicules
 FROM maintenances m
-JOIN techniciens t ON m.id_technicien = t.id_technicien
-GROUP BY t.id_technicien, t.nom, t.prenom, t.statut
+JOIN techniciens t ON m.id_technicien = t.id_technicien     -- jointure entre maintenances et techniciens selon id_technicien
+GROUP BY t.id_technicien, t.nom, t.prenom, t.statut         -- regroupé par id_technicien, nom, prénom et statut
 ORDER BY nbr_interventions ASC;
 
--- Liste des bornes de recharge par station
-SELECT * FROM bornes_de_recharge b
-JOIN stations s ON s.id_station = b.id_station;
 
--- Liste des véhicules disponibles
-SELECT * FROM vehicules
-WHERE etat = 'Disponible';
+-- 6. Liste des bornes de recharge par station
+SELECT * FROM bornes_de_recharge b                          -- sélectionne tout de la table bornes_de_recharge
+JOIN stations s ON s.id_station = b.id_station;             -- jointure entre stations et bornes_de_recharge selon id_station
 
--- Total des paiements encaissés
-SELECT SUM(montant_total) AS total_paiements
-FROM paiements;
+
+-- 7. Liste des véhicules disponibles
+SELECT * FROM vehicules                                     -- sélectionne tout de la table véhicules   
+WHERE etat = 'Disponible';                                  -- filtre pour véhicules disponibles
+
+
+-- 8. Total des paiements encaissés
+SELECT SUM(montant_total) AS total_paiements               -- somme des montants totaux des paiements de la table paiements
+FROM paiements;                             
+
+
+--9. Suppression des paiements refusés
+CREATE OR REPLACE FUNCTION supprimer_paiement_refuse()  --Créer ou remplacer une fonction pour supprimer les paiements refusés
+RETURNS TRIGGER AS $$                                   -- renvoie le triger en tant que $$ pour moins de confusion avec exemple: "'mot'"
+BEGIN
+    IF NEW.statut_paiement = 'Paiement refusé' THEN      -- si le statut du paiement est 'Paiement refusé'  
+        DELETE FROM paiements WHERE id_paiement = NEW.id_paiement; -- alors supprime le paiement de la table paiements selon id_paiement
+        RETURN NULL;
+    END IF;                 
+    RETURN NEW;                                         -- renvoie nouvelle ligne modif
+END;
+$$ LANGUAGE plpgsql;                                    -- langage utilisé = plpgsql (pour utiliser 'IF ... END IF;')
+
+CREATE TRIGGER trigger_suppr_paiement_errone            -- Execute la fonction au dessus
+AFTER INSERT OR UPDATE ON paiements                     -- Execute après insert ou màj table paiements sur chaque lignes 
+FOR EACH ROW
+EXECUTE FUNCTION supprimer_paiement_refuse();
